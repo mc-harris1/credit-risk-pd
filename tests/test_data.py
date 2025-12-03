019b590c05c288b226173f3fbf3ac7691a835a3d
Mark Harris <mark.c.harris@icloud.com>
2025-12-03 10:12:51 -0700

    Created small .csv file for test.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,11 @@
 from src.data.load_data import load_raw_loans
 
-string = "accepted_2007_to_2018Q4.csv"
+string = "test.csv"
 
 
 def test_load_raw_loans():
     df = load_raw_loans(string)
     assert not df.empty
+
+
+# TODO: Build out unittest class to set up (create sample data) and tear down (delete sample data)
